526d5107d2ac16755180b5db11d33dd516fa43c6
NStiles517 <stilesn737@gmail.com>
2026-05-11 13:15:53 -0500

    Add comparison methods to phone information class

--- phone_information.py
+++ phone_information.py
@@ -4,4 +4,26 @@ class Contact():
     self.first_name = first_name
     self.last_name = last_name
     self.phone_number = phone_number
+  def __eq__(self, other):
+    if self.first_name == other.first_name and self.last_name == other.last_name:
+      return True
+    else:
+      return False
+
+def __lt__(self, other):
+  if self.first_name < other.first_name:
+    return True
+  else:
+    return False
+
+def __gt__(self, other):
+  if self.first_name > other.first_name:
+    return True
+  else:
+    return False
+
+
+    
+  
+  
     
