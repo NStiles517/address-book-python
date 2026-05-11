f5789b376d3a5e1bc9e7eb0b43f001c51cf23dcc
NStiles517 <stilesn737@gmail.com>
2026-05-11 13:30:44 -0500

    Add import statement and merge function definition

--- algorithms.py
+++ algorithms.py
@@ -1,2 +1,4 @@
 # Write your CUSTOM search algorithm (a function) and an sorting algorithm (a function) AS DESCRIBED IN THE ASSIGNMENT INSTRUCTIONS here
 # You MAY NOT USE any pre-built python functions
+import main
+def merge(
