990ac2f194c733af3d6bf63e63af5904457f3968
NStiles517 <stilesn737@gmail.com>
2026-05-11 14:29:04 -0500

    Implement binary search and fix quick sort function
    Added binary search function and fixed syntax errors.

--- algorithms.py
+++ algorithms.py
@@ -21,13 +21,30 @@ def quick_recurse(contact_list, left, right):
     return
   pivot_position = partition(contact_list, left, right)
 
-  quick_recurse(contact_list, left, pivot_position -1)
-  quick_recurse(contact_list, pivot_position +1, right)
-
+  quick_recurse(contact_list, left, pivot_position - 1)
+  quick_recurse(contact_list, pivot_position + 1, right)
+  
 def quick_sort(contact_list):
+   quick_recurse(contact_list, 0, len(contact_list) - 1)
 
-    quick_recurse(contact_list, 0, len(contact_list) - 1)
+def binary_search(contact_list, lower_index, upper_index, target_contact);
+  if lower_index <= upper_index:
+    midpoint_index = (lower_index + upper_index) // 2
 
+    print("lower index:", lower_index)
+    print("upper index:", upper_index)
+    print("midpoint index:", mipoint_index)
+
+    if contact_list[midpoint_index] == target_contact:
+      return midpoint_index
+
+    elif (contact_list [midpoint_index] > target_contact:
+      return binary_search(contact_list, lower_index, midpoint_index - 1, target_contact)
+
+    else:
+      return binary_search(contact_list, midpoint_index + 1, upper_index, target_contact)
+
+return -1
 
 
   
